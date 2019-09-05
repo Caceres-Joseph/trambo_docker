2469db8a421e3a38aa4bb0b57b21da329b4508a4
Joseph <josephccaceres@gmail.com>
2019-09-04 18:12:19 -0600

    Args

--- Intermediate_2/script.sh
+++ Intermediate_2/script.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 
-# Compilando el Docker file
-docker build -t trambo_docker:v2 .
+# Compiling a docker file
+docker build --build-arg aws_access_key_id="AKIAXFHK42N5EBE3IE2A"  --build-arg aws_access_key_id="rE4CxHjgS77oYxDkyDfy/Rr9stjlbpHhM0vfTopl"  -t trambo_docker:v3 .
 
 # Ejecutando un container 
