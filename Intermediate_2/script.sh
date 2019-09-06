27085bf5bf2bb6177c4baae9f99bad6fbb562a61
Joseph <josephccaceres@gmail.com>
2019-09-06 14:48:54 -0600

    .

--- Intermediate_2/script.sh
+++ Intermediate_2/script.sh
@@ -2,10 +2,10 @@
 
 
 # Compiling a docker file
-docker build --build-arg aws_access_key_id="AKIAXFHK42N5EBE3IE2A"  --build-arg aws_secret_access_key="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"  -t trambo_docker:v3 .
+docker build --build-arg aws_access_key_id="AKIAXFHK42N5EBE3IE2A"  --build-arg aws_secret_access_key="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"  -t trambo_docker:v4 .
 
 # Execute a container
-docker run -d --rm trambo_docker:v2 s3 ls
+docker run -d trambo_docker:v4 s3 ls
 
 #Show logs
 docker logs
